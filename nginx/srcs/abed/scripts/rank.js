af2c892356c7f2e7421b6be86b12bc1f7234364b
hiabed <you@example.com>
2024-11-17 17:32:09 +0100

    rank on going

--- nginx/srcs/abed/scripts/rank.js
+++ nginx/srcs/abed/scripts/rank.js
@@ -7,6 +7,43 @@ import { chatPage } from "./chat.js";
 import { profileId } from "./profile.js";
 import { friendsPart } from "./friends.js";
 
+const getRank = (data) => {
+    const rank = `
+        <h3 style="text-align: center;">LEADERBOARD</h3>
+        <div id="top-ranked">
+            <div id="first-three">
+                <div id="gold">
+                    <p style="margin: 0">${data.data[0].username}</p>
+                    <div id="first-user-pic" style="background-image: url(${data.data[0].imageProfile})">
+                        <i class="fa-brands fa-pagelines leaf"></i>
+                        <i class="fa-solid fa-medal"></i>
+                    </div>
+                    <p class="fw-lighter">Score: ${data.data[0].score}</p>
+                </div>
+                <div id="silver">
+                    <p style="margin: 0">${data.data[1].username}</p>
+                    <div id="second-user-pic" style="background-image: url(${data.data[1].imageProfile})">
+                        <i class="fa-solid fa-medal"></i>
+                    </div>
+                    <p class="fw-lighter">Score: ${data.data[1].score}</p>
+                </div>
+                <div id="bronze">
+                    <p style="margin: 0">${data.data[2].username}</p>
+                    <div id="third-user-pic" style="background-image: url(${data.data[2].imageProfile})">
+                        <i class="fa-solid fa-medal"></i>
+                    </div>
+                    <p class="fw-lighter">Score: ${data.data[1].score}</p>
+                </div>
+            </div>
+        </div>
+    `
+    const rankPart = document.querySelector("#rank-part");
+    const topThree = document.createElement("div");
+    topThree.innerHTML = rank.trim();
+    rankPart.append(topThree);
+    console.log(topThree);
+}
+
 export const rankFunct = async () => {
     main.style.display = "none";
     settingPage.style.display = "none";
@@ -22,6 +59,7 @@ export const rankFunct = async () => {
         const jsonData = await response.json();
         if (jsonData.status === "success") {
             console.log("data of rank: ", jsonData);
+            getRank(jsonData);
         }
     }
 }
